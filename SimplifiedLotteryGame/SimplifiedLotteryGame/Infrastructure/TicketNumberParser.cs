4027f26eb043bbddf81977840f7bd53d956aa61d
TonyStoynev <toni.stoinev@gmail.com>
2025-10-27 22:38:28 +0200

    Add Infrastructure

---
+++ SimplifiedLotteryGame/SimplifiedLotteryGame/Infrastructure/TicketNumberParser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using SimplifiedLotteryGame.Abstractions;
+
+namespace SimplifiedLotteryGame.Infrastructure;
+
+public class TicketNumberParser(IPresentation presentation, IOptionsMonitor<LotterySettings> options) : IInputParser
+{
+    public bool TryParseInput(string input, out int output)
+    {
+        while (true)
+        {
+            if (!int.TryParse(input, out var ticketsCount))
+            {
+                presentation.Present(MessageTemplates.InvalidInputMessage);
+                input = presentation.ReadInput();
+                continue;
+            }
+
+            if (ticketsCount < options.CurrentValue.MinNumberOfTicketsPerPlayer || ticketsCount > options.CurrentValue.MaxNumberOfTicketsPerPlayer)
+            {
+                presentation.Present(MessageTemplates.InvalidTicketNumberMessage(options.CurrentValue.MinNumberOfTicketsPerPlayer, options.CurrentValue.MaxNumberOfTicketsPerPlayer));
+                continue;
+            }
+            
+            output = ticketsCount;
+            
+            return true;
+        }
+    }
+}
