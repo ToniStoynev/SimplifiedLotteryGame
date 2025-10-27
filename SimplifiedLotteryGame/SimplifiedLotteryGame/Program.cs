e297ec96dc066441f34dd6d6a2c25476a6327658
TonyStoynev <toni.stoinev@gmail.com>
2025-10-27 22:42:12 +0200

    Add configuration builder, register required services in DI container and run the program

--- SimplifiedLotteryGame/SimplifiedLotteryGame/Program.cs
+++ SimplifiedLotteryGame/SimplifiedLotteryGame/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimplifiedLotteryGame;
+using SimplifiedLotteryGame.Abstractions;
+using SimplifiedLotteryGame.Infrastructure;
 
-Console.WriteLine("Hello, World!");
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+var config = builder.Build();
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.Configure<LotterySettings>(config.GetSection("LotterySettings"));
+serviceCollection.AddSingleton<IRandomGenerator, RandomGenerator>();
+serviceCollection.AddSingleton<IInputParser, TicketNumberParser>();
+serviceCollection.AddSingleton<IPresentation, ConsolePresentation>();
+serviceCollection.AddSingleton<ILottery, SimplifiedLottery>();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var app = serviceProvider.GetRequiredService<ILottery>();
+app.Run();
+
